fa3a5debe638c17655b963cc30f81daf751f72b4
Never04eK <heket844@gmail.com>
2020-04-18 16:26:45 +0300

    Create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthorizationComponent} from './authorization/authorization.component';
+import {FormsModule} from '@angular/forms';
+import {UsersService} from './_services/users.service';
+import {CookieService} from 'ngx-cookie-service';
+import {MainComponent} from './main/main.component';
+import {HeaderComponent} from './header/header.component';
+import {RouterModule, Routes} from '@angular/router';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {AuthGuard} from "./_helpers/auth.guard";
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
+const appRoutes: Routes = [
+    {path: '', component: SignInComponent},
+    {path: 'registration', component: AuthorizationComponent},
+    {path: 'main', component: MainComponent, canActivate : [AuthGuard]},
+    {path: '**', redirectTo: ''}
+    ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthorizationComponent,
+        MainComponent,
+        HeaderComponent,
+        SignInComponent
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        AppRoutingModule,
+        FormsModule,
+        NgbModule
+    ],
+    providers: [UsersService, CookieService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
